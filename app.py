574087c530d013d94b87743b21581de846d62b33
Vojtech Brothanek <vbrothanek@brotel.cz>
2023-05-04 18:33:28 +0200

    Added function for set last values in generator.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from generator import Generator
 
 app = Flask(__name__)
 
-get_requirments_of_password = {
+dict_of_used_characters = {
     'lenght': 8,
     'capital_letters': True, 
     'letters': True, 
@@ -11,28 +11,42 @@ get_requirments_of_password = {
     'symbols': False
 }
 
-
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
+
 @app.route('/generovat', methods=['GET', 'POST'])
 def generator():
+    '''
+    The /generate page generates a password according to the user input. 
+    If a request is sent by the GET method, the /generate html page is sent and the first password is generated based on the default values. 
+    If the request is sent by POST method, the password is generated based on the user input in the fomrular on the HTML page
+    '''
     if request.method == 'GET':
-        generate = Generator(
-            capital_letters = get_requirments_of_password['capital_letters'],
-            letters = get_requirments_of_password['letters'],
-            numbers = get_requirments_of_password['numbers'],
-            symbols = get_requirments_of_password['symbols'])
-        return render_template("generovat.html", generated_password=generate.generated_password(lenght=get_requirments_of_password['lenght']))
+        char_to_passwd = Generator(
+            capital_letters = dict_of_used_characters['capital_letters'],
+            letters = dict_of_used_characters['letters'],
+            numbers = dict_of_used_characters['numbers'],
+            symbols = dict_of_used_characters['symbols'])
+        
+        generated_password = char_to_passwd.generated_password(lenght=dict_of_used_characters['lenght'])
+        return render_template("generovat.html", generated_password=generated_password)
     
     elif request.method == 'POST':
+        char_to_passwd = Generator(
+            capital_letters = dict_of_used_characters['capital_letters'],
+            letters = dict_of_used_characters['letters'],
+            numbers = dict_of_used_characters['numbers'],
+            symbols = dict_of_used_characters['symbols'])
+        
+        
         lenght = int(request.form.get('lenght'))
         capital_letters = request.form.get('capital_letters')
         letters = request.form.get('letters')
         numbers = request.form.get('numbers')
         symbols = request.form.get('symbols')
-        get_requirments_of_password.update(
+        dict_of_used_characters.update(
             {
                 'lenght': lenght,
                 'capital_letters': capital_letters, 
@@ -41,17 +55,14 @@ def generator():
                 'symbols': symbols
                 }
         )
-        generate = Generator(
-            capital_letters = get_requirments_of_password['capital_letters'],
-            letters = get_requirments_of_password['letters'],
-            numbers = get_requirments_of_password['numbers'],
-            symbols = get_requirments_of_password['symbols'])
-        return render_template("generovat.html", generated_password=generate.generated_password(lenght=get_requirments_of_password['lenght']))
+        
+        generated_password = char_to_passwd.generated_password(lenght=dict_of_used_characters['lenght'])
+        return render_template("generovat.html", generated_password=generated_password)
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
     # app.run(debug=True, host='0.0.0.0', port=5001)
     # print(get_requirments_of_password['lenght'])
     # print(get_requirments_of_password['symbols'])
--- generator.py
+++ generator.py
@@ -1,6 +1,14 @@
 import random
 import string
 
+# dict_of_used_characters = {
+#     'lenght': 8,
+#     'capital_letters': True, 
+#     'letters': True, 
+#     'numbers': True, 
+#     'symbols': False
+# }
+
 class Generator:
 
     def __init__(self, capital_letters=True, letters=True, numbers=True, symbols=False):
@@ -9,13 +17,39 @@ class Generator:
         self._password += string.digits if numbers else ""
         self._password += "!@#$%^&*(_+=-§`~\';/.,}<>?:{)" if symbols else ""
 
+    def set_last_used_characters():
+        if dict_of_used_characters["capital_letters"] == True:
+            checked_capital_letters = "checked"
+        else:
+            checked_capital_letters = ""
+        
+        if dict_of_used_characters["letters"] == True:
+            checked_letters = "Checked"
+        else:
+            checked_letters = ""
+        
+        if dict_of_used_characters["numbers"] == True:
+            checked_numbers = "checked"
+        else:
+            checked_numbers = ""
+        
+        if dict_of_used_characters["symbols"] == True:
+            checked_symbols = "checked"
+        else:
+            checked_symbols = ""
+            
+        checked_characters = {
+            "lenght": dict_of_used_characters["lenght"],
+            "capital_letters": checked_capital_letters,
+            "letters": checked_letters,
+            "numbers": checked_numbers,
+            "symbols": checked_symbols
+        }
+        return checked_characters
 
     def generated_password(self, lenght=8):
             return "".join([random.choice(self._password) for l in range(lenght)])
     
 
-# if __name__ == '__main__':
-#      generate = Generator(capital_letters=True, letters=True, numbers=True, symbols=True)
-#      print(generate.generated_password(lenght=8))
-     
+
     
